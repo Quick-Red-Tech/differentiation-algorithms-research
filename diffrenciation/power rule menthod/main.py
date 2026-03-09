ff5c12d267a98da1e17a8dabc0f35f23ccf4efc7
chisom life eke <chisomlifeeke@gmail.com>
2026-03-09 18:14:09 +0100

    licenses update

---
+++ diffrenciation/power rule menthod/main.py
@@ -0,0 +1,13 @@
+# Copyright 2026 QuickRed Tech
+# Licensed under the Apache License, Version 2.0
+
+# This code is part of the QuickRed Tech project and is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+#imports of libraries
+
+print("Welcome to the Power Rule Differentiation Method!")
+print("example of equations that can be differentiated using this method: 2X^3-3X^3+2")
+print("the answer to this question is: 6X^2-9X^2")
+# Function to differentiate using the power rule
